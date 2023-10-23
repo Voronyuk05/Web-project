9b1c8c84670aed1e7af9ba0521b8508dc66956c2
Ivan <vandoslando@gmail.com>
2023-10-23 12:28:35 +0300

    start project

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './js/footer';
-import './js/header';
-import './js/main-section';
-import './js/modal';
-import './js/pagination';
-import './js/closing-modal-esc';
-import './js/change-theme';
